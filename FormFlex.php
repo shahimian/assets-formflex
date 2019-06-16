31eaae9d86e9190ff1feeebd54d48e9b69ae90d1
a.shahimian <shahimian@gmail.com>
2019-06-16 15:55:39 +0430

    initial project

---
+++ FormFlex.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\assets\formflex;
+
+use yii\web\AssetBundle;
+
+class FormFlex extends AssetBundle {
+
+    public $sourcePath = "@app/assets/formflex/assets";
+
+    public $css = [
+        'formflex.css',
+    ];
+
+}
